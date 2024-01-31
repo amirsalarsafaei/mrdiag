be99da3f9915f367ddcae6d2d2f14b3399da05f2
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2024-01-31 19:50:46 +0330

    fix

--- diag_report/controller.py
+++ diag_report/controller.py
@@ -23,3 +23,40 @@ def get_phone_image(report: DiagReport) -> Optional[str]:
     if len(gis.results()) == 0:
         return None
     return gis.results()[0].url
+
+
+def get_android_name(version):
+    # Android version names as of the last update in early 2023
+    version_names = {
+        "1.5": "Cupcake",
+        "1.6": "Donut",
+        "2.0": "Eclair",
+        "2.1": "Eclair",
+        "2,2": "Froyo",
+        "2.3": "Gingerbread",
+        "3.0": "Honeycomb",
+        "3.1": "Honeycomb",
+        "3.2": "Honeycomb",
+        "4.0": "Ice Cream Sandwich",
+        "4.1": "Jelly Bean",
+        "4.2": "Jelly Bean",
+        "4.3": "Jelly Bean",
+        "4.4": "KitKat",
+        "5.0": "Lollipop",
+        "5.1": "Lollipop",
+        "6.0": "Marshmallow",
+        "7.0": "Nougat",
+        "7.1": "Nougat",
+        "8.0": "Oreo",
+        "8.1": "Oreo",
+        "9": "Pie",
+        "10": "Queen Cake",
+        "11": "Red Velvet Cake",
+        "12": "Snow Cone",
+        "12.1": "Snow Cone v1.1",
+        "13": "Tiramisu"
+    }
+
+    # Return the corresponding name; if not found, return "Unknown"
+    return version_names.get(str(version), "Unknown")
+
