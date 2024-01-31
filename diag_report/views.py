6b6dd772291be66b0c9b2a42adddf35d1acb9aa6
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2024-01-31 21:22:50 +0330

    fix

--- diag_report/views.py
+++ diag_report/views.py
@@ -42,7 +42,7 @@ def submit_report(request, *args, **kwargs):
     serializer = SubmitDiagReportSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     print(serializer.validated_data['ticket'])
-    report = get_object_or_404(DiagReport, ticket=serializer.validated_data['ticket'])
+    report = get_object_or_404(DiagReport, ticket_ticket=serializer.validated_data['ticket'])
     try:
         create_report_addon(report)
         report.submitted = True
